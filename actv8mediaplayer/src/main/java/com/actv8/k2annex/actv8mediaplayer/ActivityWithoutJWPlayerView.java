493941cffdf43230c429531c385bcb5e275cac0e
dnyaesh <dnyanesh.m88@gmail.com>
2020-02-06 14:37:06 +0530

    Test Activity Changes

---
+++ actv8mediaplayer/src/main/java/com/actv8/k2annex/actv8mediaplayer/ActivityWithoutJWPlayerView.java
@@ -0,0 +1,14 @@
+package com.actv8.k2annex.actv8mediaplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityWithoutJWPlayerView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_without_jwplayer_view);
+    }
+}
